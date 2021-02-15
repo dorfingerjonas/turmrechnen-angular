0b2411851db3a67b762d2994bb62503dc7dd624f
dorfingerjonas <jonas.dorfinger@gmx.at>
2021-02-15 17:58:54 +0100

    create component result-row

---
+++ src/app/components/result-row/result-row.component.ts
@@ -0,0 +1,16 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-result-row',
+  templateUrl: './result-row.component.html',
+  styleUrls: ['./result-row.component.scss']
+})
+export class ResultRowComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+}
