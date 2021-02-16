cbc3c0fe97ea6d09549b537a3bf54b1e51c7e0fe
dorfingerjonas <jonas.dorfinger@gmx.at>
2021-02-16 11:03:55 +0100

    remove result-row component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,10 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from './app.component';
-import {ResultRowComponent} from './components/result-row/result-row.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ResultRowComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule
